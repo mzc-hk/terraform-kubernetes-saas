10765c2e3ad171b4cd82b5886c5ba7ede2b697f4
FlyingTwigs <dorerudy@gmail.com>
2023-11-17 10:16:32 +0000

    Add terraform providers

---
+++ versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    helm       = "~> 2.0"
+    kubernetes = "~> 2.0, != 2.12"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.0"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = "~> 1.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 3"
+    }
+  }
+}
