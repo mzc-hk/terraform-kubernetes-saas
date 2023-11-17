051f47d17bf969314b94c88e17dbfe7776c162d3
FlyingTwigs <dorerudy@gmail.com>
2023-11-17 18:14:50 +0000

    Add groundcover from source and change the required providers

--- versions.tf
+++ versions.tf
@@ -1,27 +1,21 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
-    helm       = "~> 2.0"
-    kubernetes = "~> 2.0, != 2.12"
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.0"
+    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.0"
     }
-    flux = {
-      source  = "fluxcd/flux"
-      version = "~> 1.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.19.0"
     }
   }
 }
